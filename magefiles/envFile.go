beaa7f606987e79c1c55a3b0838a6a89f401d8b2
Yaakov Yehezkel <YehezkelY@aetna.com>
2022-08-18 13:33:21 -0400

    Adding env support to skip on k8s test (for now) still in progress

---
+++ magefiles/envFile.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type envList struct {
+	timezone       string
+	cciApiInterval string
+	ghRepos        string
+	ghBranches     string
+	cciToken       string
+}
+
+func (e *envList) TokenInit(token string) {
+	e.cciToken = stringOrDefault(token, "noMoreSecrets")
+}
+
+func (e *envList) SetEnvVars() {
+	envVars := strings.Split(e.ToEnvFile(), "\n")
+
+	for _, envVar := range envVars {
+		kv := strings.Split(envVar, "=")
+		if kv[0] != "" && kv[1] != "" {
+			os.Setenv(kv[0], kv[1])
+		}
+	}
+}
+
+func (e *envList) WriteEnvFile(outFile string) error {
+	_, err := os.Stat(outFile)
+	if err == nil {
+		return fmt.Errorf("file already exist")
+	}
+
+	err = ioutil.WriteFile(outFile, []byte(e.ToEnvFile()), 0666)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *envList) ToEnvFile() string {
+	var data strings.Builder
+	if e.cciApiInterval != "" {
+		data.WriteString(fmt.Sprintf("CIRCLECI_API_INTERVAL=%s", e.cciApiInterval))
+	}
+	if e.cciToken != "" {
+		data.WriteString(fmt.Sprintf("\nCIRCLECI_TOKEN=%s", e.cciToken))
+	}
+	if e.ghBranches != "" {
+		data.WriteString(fmt.Sprintf("\nGITHUB_BRANCH=%s", e.ghBranches))
+	}
+	if e.timezone != "" {
+		data.WriteString(fmt.Sprintf("\nTZ=%s", e.timezone))
+	}
+	if e.ghRepos != "" {
+		data.WriteString(fmt.Sprintf("\nGITHUB_REPOSITORY=%s", e.ghRepos))
+	}
+	return data.String()
+}
+
+func NewEnvList() envList {
+	return envList{
+		timezone:       "America/New_York",
+		cciApiInterval: "300",
+		ghRepos:        "aetna-digital-infrastructure/DSSP-Developer-Portal,aetna-digital-infrastructure/dfp-gitops-package-ccloud-definitions,aetna-digital-infrastructure/rally-service",
+		ghBranches:     "master,main",
+		cciToken:       "noMoreSecrets",
+	}
+}
